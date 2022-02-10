17c70dee14e3fdca639bba108343ddd5b99559bb
이승준 <lsj135779@gmail.com>
2022-02-10 17:14:24 +0900

    03.make Modal

--- custom-component/src/component/Modal.js
+++ custom-component/src/component/Modal.js
@@ -1,5 +1,72 @@
 import styled from "styled-components";
+import { useState } from "react";
+
+const Container = styled.div`
+  height: 100%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  background: #4701cd;
+  color: white;
+  height: 50px;
+  border-radius: 20px;
+  cursor: grab;
+  border: none;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
+
+const View = styled.div`
+  background-color: white;
+  width: 300px;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  margin: 100px;
+  color: #4701cd;
+  text-align: center;
+  border-radius: 10px;
+  .xbtn {
+    cursor: pointer;
+    padding-top: 5px;
+    margin-bottom: 30px;
+  }
+`;
 
 export const Modal = () => {
-  return <div>Modal</div>;
+  const [isOn, setIsOn] = useState(false);
+
+  const clickModal = () => {
+    setIsOn(!isOn);
+  };
+
+  return (
+    <Container>
+      <Button onClick={clickModal}>{isOn ? "Opened" : "Open Modal"}</Button>
+      {isOn && (
+        <Backdrop onClick={clickModal}>
+          <View onClick={(e) => e.stopPropagation()}>
+            <span className="xbtn" onClick={clickModal}>
+              &times;
+            </span>
+            HELLO CODESTATES!
+          </View>
+        </Backdrop>
+      )}
+    </Container>
+  );
 };
